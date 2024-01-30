b98d96c019132b9d5bbc3ef248ba78947276f822
Xin Deng <xind1470@gmail.com>
2024-01-30 15:54:53 -0800

    citation count is printing

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,34 +1,19 @@
 from bs4 import BeautifulSoup
-
 import requests
 
-
 def get_citations_needed_count(url):
-    url="https://en.wikipedia.org/wiki/Fatigue"
-    response=requests.get(url)
-    results = parse(response.text)
-    soup = BeautifulSoup(results, "html.parser")
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
     citations_needed = soup.find_all(class_='noprint Inline-Template Template-Fact')
+
+    with open("wiki.txt", "w") as f:
+        f.write(response.text)
+
     return len(citations_needed)
 
-    with open("courses.txt","w") as f:
-      f.write(results)
 
-
-
-def get_citations_needed_report():
-    url="https://en.wikipedia.org/wiki/Fatigue"
-    response=requests.get(url)
-    results = parse(response.text)
-    soup = BeautifulSoup(results, "html.parser")
-    citations_needed = soup.find_all(class_='noprint Inline-Template Template-Fact')
-    report = "These texts need citations: \n\n"
-
-    for citation in citations_needed:
-        parent = citation.find_parent()
-        citation_report += f"{parent.text.strip()}\n\n"
-
-    return citation_report
-
-
+if __name__ == "__main__":
+    url = "https://en.wikipedia.org/wiki/Fatigue"
+    count = get_citations_needed_count(url)
+    print(f"Number of citations needed: {count}")
 
